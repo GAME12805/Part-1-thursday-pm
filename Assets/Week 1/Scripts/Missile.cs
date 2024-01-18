e13ed7b0a045e9426ab503071c1478f0ebae94cf
SHERNET\barryki <kit.barry@sheridancollege.ca>
2024-01-18 14:20:03 -0500

    Task 1 complwrw missile collisions

--- Assets/Week 1/Scripts/Missile.cs
+++ Assets/Week 1/Scripts/Missile.cs
@@ -1,13 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Missile : MonoBehaviour
 {
     public float speed = 10f;
+    Rigidbody2D rigidbody;
+    Vector2 direction = new Vector2(10, 0);
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
 
     void Update()
     {
-        transform.Translate(speed * Time.deltaTime, 0, 0);
+        //transform.Translate(speed * Time.deltaTime, 0, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + direction * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Missile hit something");
+        Destroy(gameObject);
     }
 }
