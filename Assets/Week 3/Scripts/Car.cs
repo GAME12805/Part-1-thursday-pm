f508a894135def780aaa7d6b966e8926753ed7df
SHERNET\barryki <kit.barry@sheridancollege.ca>
2024-01-25 14:51:39 -0500

    Task 3 complete: added car physics

---
+++ Assets/Week 3/Scripts/Car.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    float forwardInput;
+    float steeringInput;
+    public float forwardSpeed = 500;
+    public float steeringSpeed = 200;
+    Rigidbody2D rigidbody;
+    public float maxSpeed = 450;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        steeringInput = Input.GetAxis("Horizontal");
+    }
+    private void FixedUpdate()
+    {
+        rigidbody.AddTorque(steeringInput * -steeringSpeed * Time.deltaTime);
+        Vector2 force = transform.up * forwardInput * forwardSpeed * Time.deltaTime;
+        if(rigidbody.velocity.magnitude < maxSpeed)
+        {
+            rigidbody.AddForce(force);
+        }
+    }
+}
